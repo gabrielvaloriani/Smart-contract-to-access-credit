dddd1af85527e3f4f6eab6786c787b317bbcc1bd
gabrielvaloriani <gabriel.valoriani@gmail.com>
2024-05-22 11:41:02 +0200

    Changes to respond the coach's feedback.

--- LoanManager.sol
+++ LoanManager.sol
@@ -23,6 +23,9 @@ contract LoanManager {
 
     // Function to create a new loan
     function createLoan(address _borrower, uint _amount, uint _interestRate, uint _loanPeriod) external {
+        require(_amount > 0, "Loan amount must be greater than zero.");
+        require(_loanPeriod > 0, "Loan period must be greater than zero.");
+        
         uint startTime = block.timestamp;
         uint endTime = startTime + (_loanPeriod * 1 days);
 
@@ -31,45 +34,44 @@ contract LoanManager {
         loans[_borrower].push(newLoan);
     }
 
+    // Internal function to get the active loan of a user
+    function _getActiveLoan(address _user) internal view returns (Loan storage) {
+        require(hasActiveLoan(_user), "The loan for this user does not exist.");
+        return loans[_user][loans[_user].length - 1];
+    }
+
     // Function to get the interest rate of a loan
     function getInterestRate() external view returns (uint) {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        return loans[msg.sender][loans[msg.sender].length - 1].interestRate;
+        return _getActiveLoan(msg.sender).interestRate;
     }
 
     // Function to get the interest amount of a loan
     function getInterestAmount() external view returns (uint) {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        Loan storage userLoan = loans[msg.sender][loans[msg.sender].length - 1];
+        Loan storage userLoan = _getActiveLoan(msg.sender);
         return FinancialLibrary.calculateInterest(userLoan.amount, userLoan.interestRate, userLoan.loanPeriod);
     }
 
     // Function to get the end time of a loan
     function getLoanEndTime() external view returns (uint) {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        return loans[msg.sender][loans[msg.sender].length - 1].endTime;
+        return _getActiveLoan(msg.sender).endTime;
     }
 
     // Function to get the status of a loan
     function getLoanStatus() external view returns (string memory) {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        Loan storage userLoan = loans[msg.sender][loans[msg.sender].length - 1];
-        if (userLoan.isActive) {
-            if (block.timestamp < userLoan.endTime && !userLoan.isPaid) {
-                return "active";
-            } else if (block.timestamp >= userLoan.endTime && !userLoan.isPaid) {
-                return "expired";
-            } else if (userLoan.isPaid) {
-                return "paid";
-            }
+        Loan storage userLoan = _getActiveLoan(msg.sender);
+        if (block.timestamp < userLoan.endTime && !userLoan.isPaid) {
+            return "active";
+        } else if (block.timestamp >= userLoan.endTime && !userLoan.isPaid) {
+            return "expired";
+        } else if (userLoan.isPaid) {
+            return "paid";
         }
         return "undefined state";
     }
 
     // Function to allow users to repay a loan
     function repayLoan() external payable {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        Loan storage userLoan = loans[msg.sender][loans[msg.sender].length - 1];
+        Loan storage userLoan = _getActiveLoan(msg.sender);
         uint penalty = 0;
         if (block.timestamp > userLoan.endTime) {
             penalty = FinancialLibrary.calculatePenalty(userLoan.amount, userLoan.endTime, block.timestamp);
@@ -77,14 +79,18 @@ contract LoanManager {
         require(msg.value >= userLoan.amount + penalty, "The payment amount is not sufficient.");
         require(!userLoan.isPaid, "The loan has already been paid.");
 
-        userLoan.lender.transfer(msg.value);
+        uint excessPayment = msg.value - (userLoan.amount + penalty);
+        if (excessPayment > 0) {
+            payable(msg.sender).transfer(excessPayment);
+        }
+
+        userLoan.lender.transfer(userLoan.amount + penalty);
         userLoan.isPaid = true;
     }
 
     // Function to cancel a loan
     function cancelLoan() external {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        Loan storage userLoan = loans[msg.sender][loans[msg.sender].length - 1];
+        Loan storage userLoan = _getActiveLoan(msg.sender);
         require(!userLoan.isPaid, "The loan has already been paid.");
 
         userLoan.isActive = false;
@@ -92,8 +98,7 @@ contract LoanManager {
 
     // Function to get loan information
     function getLoanDetails() external view returns (address, address, uint, uint, uint, uint, uint, bool, bool) {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        Loan storage userLoan = loans[msg.sender][loans[msg.sender].length - 1];
+        Loan storage userLoan = _getActiveLoan(msg.sender);
         return (
             userLoan.lender,
             userLoan.borrower,
@@ -109,8 +114,7 @@ contract LoanManager {
 
     // Function to get the penalty amount for a late loan
     function getPenaltyAmount() external view returns (uint) {
-        require(hasActiveLoan(msg.sender), "The loan for this user does not exist.");
-        Loan storage userLoan = loans[msg.sender][loans[msg.sender].length - 1];
+        Loan storage userLoan = _getActiveLoan(msg.sender);
         require(block.timestamp > userLoan.endTime && !userLoan.isPaid, "The loan has not yet come due or been paid.");
 
         uint penalty = FinancialLibrary.calculatePenalty(userLoan.amount, userLoan.endTime, block.timestamp);
